b72dcb34dfe2810cfe7c726771d1f317860f6c06
frozenkp <csjh21010@gmail.com>
2022-11-08 03:39:59 +0800

    20221107

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('nav').find('a').click(function () {
         let target = $(this).data('target');
-        let o = $(target).offset().top - 86;
+        let o = $(target).offset().top - 51;
         $('html,body').animate({
             scrollTop: o
         }, 300);
