5c950526fa1960a3fcede400f26bd98ded192003
peterchen <peterchen2017125@gamil.com>
2022-11-11 14:56:05 +0800

    20221111

--- js/all.js
+++ js/all.js
@@ -20,9 +20,67 @@ Vue.createApp({
             }
             ],
             products: [{
-                title: "抗菌洗手乳",
-                
-            }]
+                title: '抗菌洗手乳',
+                content: '飼養在深山裡的放山雞，本農場將雞高溫烘烤製成快速又方便的調理包，加熱僅可使用',
+                img: './img/',
+                price: 200,
+                amountShow: 0,
+            },
+            {
+                title: '一般醫療口罩',
+                content: '飼養在深山裡的放山雞，本農場將雞高溫烘烤製成快速又方便的調理包，加熱僅可使用',
+                img: './img/',
+                price: 180,
+                amountShow: 0,
+            },
+            {
+                title: '特殊N95口罩',
+                content: '飼養在深山裡的放山雞，本農場將雞高溫烘烤製成快速又方便的調理包，加熱僅可使用',
+                img: './img/',
+                price: 50,
+                amount: 0,
+                amountShow: 0,
+            },
+            {
+                title: '滾筒衛生紙',
+                content: '香甜蘋果色澤紅潤，無添加臘模清洗後即可食用',
+                img: './img/',
+                price: 60,
+                amountShow: 0,
+            },
+            {
+                title: '體溫計',
+                content: '夏日炎炎，消暑必備的西瓜一顆60元數量有限，要買要快！',
+                img: './img/',
+                price: 99,
+                amountShow: 0,
+            },
+            {
+                title: '冷凍健康餐',
+                content: '有機菠菜農場栽種，無化學農藥讓您吃的健康又安心',
+                img: './img/',
+                price: 160,
+                amountShow: 0,
+            },
+            ],
         }
+    }, methods: {
+        minus(product) {
+            product.amountShow--
+            product.amountShow = (product.amountShow < 1) ? 0 : product.amountShow
+        },
+        plus(product) {
+            product.amountShow++
+            product.amountShow = (product.amountShow > 9) ? 9 : product.amountShow
+        },
+    },
+    computed: {
+        sub() {
+            return this.products.map(data => data.price * data.amountShow)
+        },
+        total() {
+            return this.sub.reduce((a, b) => a + b)
+        },
     }
+
 }).mount("#app");
